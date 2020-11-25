927e3693491b0c4e5c33cafe87bccee80992ba0e
mikola shamshorik <mikola.shamshorik1@gmail.com>
2020-11-25 20:05:38 +0200

    model

---
+++ diary/diary/Models/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace diary.Models
+{
+    public class Position
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+    }
+}
---
+++ diary/diary/Models/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace diary.Models
+{
+    public class Worker
+    {
+        public int Id { set; get; }
+        public string FirstName { set; get; }
+        public string MiddlName { set; get; }
+        public string LastName { set; get; }
+        public int Position_Id { set; get; }
+        public double HourlyPayment { set; get; }
+    }
+}
