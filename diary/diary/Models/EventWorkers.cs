a0eb86b4b7a2af7b2ebd6223b2d48fb9b8d1f6a4
mikola shamshorik <mikola.shamshorik1@gmail.com>
2020-11-25 20:54:09 +0200

    Event

---
+++ diary/diary/Models/EventWorkers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace diary.Models
+{
+    public class EventWorkers
+    {
+        public int Id { set; get; }
+        public int Worker_Id { set; get; }
+        public double Hours { set; get; }
+
+
+    }
+}
