c7b7136da96c05a219b94b133ebfc36f8c16888c
Vitalik Stolaruk <Vetalstol@gmail.com>
2020-11-25 20:07:40 +0200

    Add methods, add table, addWorkContext, update homeController

---
+++ diary/diary/Models/WorkContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace diary.Models
+{
+    public class WorkContext : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; }
+
+        public WorkContext(DbContextOptions<WorkContext> options)
+            : base(options)
+        {
+
+            Database.EnsureCreated();
+        }
+    }
+}
