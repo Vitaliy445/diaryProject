fdc3ec252b576360dc1cd552fa88d66beba54635
mikola shamshorik <mikola.shamshorik1@gmail.com>
2020-12-24 19:31:28 +0200

    form-inner

---
+++ diary/diary/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using diary.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace diary.Controllers
+{
+    public class AccountController : Controller
+    {
+
+    }
+}
